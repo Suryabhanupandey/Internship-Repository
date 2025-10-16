daba7caa2fefd5117ff23930bb3cb3ab20024c91
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-10-15 22:56:29 -0700

    First JavaScript Code

---
+++ JAVASCRIPT/DAY1/intro.js
@@ -0,0 +1,9 @@
+/* JavaScript:
+                        JavaScript is a scripting language used to make web pages interactive and dynamic.
+                    It runs directly in the browser, allowing you to create effects like form validation, animations, sliders, and more.
+
+*/
+
+
+console.log("This is my first javaScript code ");
+console.log("Welcome to my Repository");
