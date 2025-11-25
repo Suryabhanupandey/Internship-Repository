985b3f8518dd211bdf523bbe77cf822709cc36ba
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-11-24 18:14:49 -0800

    Intro Java

---
+++ Java/DAY1/First.java
@@ -0,0 +1,19 @@
+// Java Introduction
+// 1.Java was develop by Sun microSystems & now it taken by Oracle.
+// 2.What is Java? - Java is a technology
+// technology - Sophisticated Programming Language & Sophisticated Programing Constructs.
+// Programing Constructs
+// 1.Systax
+// 2.Library
+// 3.Programming Rule
+
+// 3.Java is a Platform independent language
+// WORA - Write One Run Anywhere
+
+public class First{
+       public static void main(String[] args)
+       {
+        System.out.println("Hello....World!");
+       }
+
+}
