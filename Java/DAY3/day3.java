624bd9563a0a06a23721d2ecdca153d871b12dd3
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-11-26 14:59:49 -0800

    Day 2

---
+++ Java/DAY3/day3.java
@@ -0,0 +1,19 @@
+//Java is a Case sensitive language.
+public class day3 {
+
+ 		public static void main(String args[])
+	{
+		byte a = 10;
+		System.out.println("a = " +a);
+		
+		a = 014; 					  //Octal
+		System.out.println("a = " +a);
+
+		a = 0xA; 					//hexadecimal
+		System.out.println("a = " +a);
+
+		a = 0xa; 					//hexadecimal
+		System.out.println("a = " +a);
+
+	}
+}
