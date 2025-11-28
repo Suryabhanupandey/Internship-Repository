644217a85b56844daa57ccc9daa613a8894c262e
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-11-27 20:12:34 -0800

    Day 4

---
+++ Java/DAY4/DayFourSec.java
@@ -0,0 +1,18 @@
+public class DayFourSec
+
+{
+	public static void  main(String args[])
+		{
+
+		int a = 100; 
+		int b = 50;
+		System.out.println((b<a)?"A is greater " : "B is Greater");
+
+		String proof = false? "U hava Written Flase": "You have written true";
+		System.out.println(proof);
+
+		}
+
+
+
+}
