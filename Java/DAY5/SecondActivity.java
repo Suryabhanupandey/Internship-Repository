5fb9fc22d1e5b08347b4c4c398ac7f9f38a45b3e
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-11-28 18:06:23 -0800

    Day 5

---
+++ Java/DAY5/SecondActivity.java
@@ -0,0 +1,21 @@
+public class SecondActivity
+
+{
+
+	public static void main(String args[])
+	{
+	int a = 10;
+	System.out.println("Post-Increement :: "+ a++);
+
+	int b = 10;
+	System.out.println("Pre-Increement :: " + ++b);
+
+	//Short hand operator/ compound operator
+	int c =10;
+		c*=5;
+		System.out.println("Short Hand Operator c :: " + c);
+
+	}
+
+
+}
---
+++ Java/DAY5/ThirdActivity.java
@@ -0,0 +1,14 @@
+public class ThirdActivity{
+
+  			public static void main(String args[]);
+				{
+					int value = Integer.parseInt(args[0]);
+
+if(value % 2 == 0){
+
+					System.out.println(value + " is Even");
+}
+
+
+				}
+}
