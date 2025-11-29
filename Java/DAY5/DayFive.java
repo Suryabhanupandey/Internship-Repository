389c0edf3ad5458e660b10879ce702304e1ca4d3
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-11-28 18:05:49 -0800

    Day 5

---
+++ Java/DAY5/DayFive.java
@@ -0,0 +1,17 @@
+public class DayFive
+{
+	public static void main( String args[])
+		{
+
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println("Result of Bitwise & = " + (a&b));
+		System.out.println("Result of Bitwise | = " + (a|b));
+
+		System.out.println("Result of Bitwise ^ = " + (a^b));
+
+
+		}
+
+
+}
