c28e44db0aed0b04f49534530fba7c03abbe2089
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-11-29 17:52:43 -0800

    Day3 ThirdFile

---
+++ Java/DAY3/ThirdFile.java
@@ -0,0 +1,41 @@
+ public class ThirdFile{
+    
+	public static void main(String args[])
+		{
+			//float		
+	
+			float a = 10.02f;     		// F suffix is mandatory hare
+			System.out.println("a is float = " +a);
+
+  			 a = 10.32F;
+			System.out.println("a = " +a);
+
+			// Double
+
+			double b = 10.001; //d / D is optional
+			System.out.println("b is double = " +b);
+			
+			b = 1.25e-1D;
+			System.out.println("b is double = " +b);
+			
+			b = 1.35e-2d;
+			System.out.println("b is double = " +b);
+
+			// boolean  
+			// 8 bits
+			//true>00000001
+			//false> 00000000
+
+			boolean c = false;
+			System.out.println("c is boolean = " +c);
+		
+			c = true;
+			System.out.println("c is boolean = " +c);
+
+
+
+
+		}
+
+
+}
