98deee36225b2c2746c155e184c74ff3033a893b
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-11-30 16:48:21 -0800

    Day 2

---
+++ Java/DAY2/DataTypesExample.java
@@ -0,0 +1,19 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+
+        // Primitive data types
+        int age = 20;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean isPass = true;
+
+        // Non-primitive data type
+        String name = "Surya";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPass);
+    }
+}
