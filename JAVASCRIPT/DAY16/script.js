05f579fe42eab67deb86237aa0cf25fa9e866cf9
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-12-01 15:30:24 -0800

    Day 16

---
+++ JAVASCRIPT/DAY16/script.js
@@ -0,0 +1,15 @@
+console.log("Hello World"); //synchronous Code
+setTimeout(() =>{  //Asynchronous code
+    console.log("this is a Asynchronous Code");    
+} ,5000)
+console.log("Surya");
+
+
+// Create a function
+function delaycode(){
+    console.log("hello World");
+}
+
+setTimeout(delaycode,5000);
+
+ 
