49bd09ff897335775e3809bd6a1e80ae6031ea61
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-12-03 15:31:10 +0530

    Day 7 loop

---
+++ Java/DAY7/ThirdActivity.java
@@ -0,0 +1,20 @@
+public class ThirdActivity
+
+{
+	public static void main(String args[])
+
+			{
+				//int a = 1;
+				//for(; a<=10 ; )
+				for (int a=1,j=1; a<=10; a++,j+=2) 
+				{
+					System.out.println(a +"   "+ j);
+					//a++;
+
+				}
+					//System.out.println("a = "  + a);
+
+			}
+
+
+}
