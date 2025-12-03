00368b3cdd05239ee4433d07efb13500bdc77961
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-12-03 15:29:42 +0530

    Day 7

---
+++ Java/DAY7/FirstActivity.java
@@ -0,0 +1,33 @@
+public class FirstActivity
+
+
+
+
+{
+	public static void main(String args[])
+
+			{
+				int value = Integer.parseInt(args[0]);
+				switch(value)
+				{
+
+					case 1 :
+						System.out.println("FIrst Division");
+						break;
+					case 2:
+						System.out.println("Second   division");
+						break;
+					case 3:
+						System.out.println("Third division");
+						break;
+					default:
+						System.out.println("Failed");
+						
+				}
+
+
+
+			}
+
+
+}
