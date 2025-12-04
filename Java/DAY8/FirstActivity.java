162ca91652c8e5c33535e667daa9f52f31d4f3a1
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-12-04 15:16:08 +0530

    Day 8

---
+++ Java/DAY8/FirstActivity.java
@@ -0,0 +1,22 @@
+public class FirstActivity
+{
+	public static void main(String args[])
+
+		{
+			int value =Integer.parseInt(args[0]);
+			long result = 1;
+			
+			for(int i=1; i<=value; i++)
+			{
+				result = result*i;
+			}
+		
+			System.out.println("The Factorial of " +value+ " is :: "+result);
+
+		}
+
+
+
+
+
+}
