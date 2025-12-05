74cfe44625e66e351dbf08aee4d8b6941b315fde
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-12-05 14:43:14 +0530

    day8

---
+++ Java/DAY8/ThirdActivity.java
@@ -0,0 +1,24 @@
+public class ThirdActivity
+{
+	public static void main(String args[])
+
+		{
+ 
+ 			int a= 1;
+			while(a<=10)
+			{
+	
+				System.out.println(a);
+				a++;	
+	
+			}
+		
+ 
+
+		}
+
+
+
+
+
+}
