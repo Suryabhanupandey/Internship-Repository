a80b5c6eeb0224850b170e58efad19badd3070d2
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-12-09 14:38:44 +0530

    Object

---
+++ Java/DAY12/FirstActivity.java
@@ -0,0 +1,28 @@
+ class A {
+
+		//return type
+		
+
+		int display(int l,int b){
+
+			 int area = l*b;
+				return area;
+
+
+			}
+
+}
+public class FirstActivity{
+	public static void main(String agrs[])
+
+		{
+		A obj=new A();
+
+		 System.out.println("Area = " +obj.display(10,20)); //actual parameter
+
+ 
+
+
+		}
+
+}
