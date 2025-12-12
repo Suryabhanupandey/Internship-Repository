5f73f2aba6f79ef6820ffd5e2e3da10834ee4ebc
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2025-12-12 15:06:07 +0530

    Day11

---
+++ Java/DAY11/Student.java
@@ -0,0 +1,27 @@
+ class Student {
+
+		String name;
+		int age;
+
+		void display(){
+
+			System.out.println("Name : "+ name);
+			System.out.println("Age : " +age);
+
+
+			}
+
+	public static void main(String agrs[])
+
+		{
+		Student s1=new Student();
+
+		s1.name= "Surya Bhanu Pandey";
+		s1.age = 20;
+
+		s1.display();	
+
+
+		}
+
+}
