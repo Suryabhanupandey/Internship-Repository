c9b226e026b44a55e591861d8bedd8af1cdd72d5
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2026-01-04 19:22:13 +0530

    package

--- Java/Package/FifthActivity.java
+++ Java/Package/FifthActivity.java
@@ -1,12 +1,38 @@
-class A {
+package com.app;
 
-    	public static void main(String[] args) {
-           	System.out.println("Hello ... A");
-    		}
+import java.util.*;
+
+class Room {
+
+    boolean isPrime(int number) {
+
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
- class FifthActivity {
-    public static void main(String[] args) {
-           System.out.println("Hello ...");
+public class FifthActivity {
+
+    public static void main(String args[]) {
+
+        Room obj = new Room();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+        int value = scan.nextInt();
+
+        if (obj.isPrime(value)) {
+            System.out.println("This is a Prime Number :: True");
+        } else {
+            System.out.println("This is NOT a Prime Number :: False");
+        }
     }
 }
