b03e25eafa01238eb974e3a029a4c85cd47ca783
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2026-01-04 19:22:52 +0530

    Day22

---
+++ Java/DAY22/ThirdActivity.java
@@ -0,0 +1,21 @@
+package com.app;
+
+import java.io.*;
+
+
+public class thirdActivity
+	{
+
+		Scanner scan = new Scanner(System.in);
+		
+
+		System.out.println("Enter The First Word");
+		String word1 =scan.nextLine().toUpperCase();
+		System.out.println("Enter THe Second")
+
+
+
+
+
+
+	}
