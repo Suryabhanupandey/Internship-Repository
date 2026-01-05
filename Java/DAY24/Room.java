f74ce654fd35896bacb08ed198108b6897f99e50
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2026-01-05 13:07:04 +0530

    Serialization

---
+++ Java/DAY24/Room.java
@@ -0,0 +1,16 @@
+package Com.app;
+
+import java.io.*;
+
+public class Room implements Serializable {
+
+    int length;
+    int breadth;
+
+     
+    public Room(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+
+}
