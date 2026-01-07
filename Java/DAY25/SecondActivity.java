d84d2d8def246d473e91dd60ce7f05a6c887bdfd
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2026-01-07 11:14:08 +0530

    Thread

---
+++ Java/DAY25/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.app;
+
+
+class MyTask implements Runnable
+{
+
+	public void run()
+		{
+			for(int i=1;i<=10;i++)
+				{
+				System.out.println("i " +i+" Current Threads Name :: "+Thread.currentThread().getName());
+			}
+		}
+
+}
+
+
+public class SecondActivity
+{
+
+	public static void main(String args[])
+		{
+			MyTask task = new MyTask();
+			Thread t1 = new Thread(task);
+
+			t1.setName("Surya");
+
+			t1.start();
+
+		}
+
+}
