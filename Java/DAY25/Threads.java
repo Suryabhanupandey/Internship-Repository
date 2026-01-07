6297bf50dabf671a6341e99d135cf1610801091c
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2026-01-07 11:15:52 +0530

    Thread

---
+++ Java/DAY25/Threads.java
@@ -0,0 +1,15 @@
+package com.app;
+
+
+public class Threads
+{
+		public static void main(String args[])
+
+		{
+			System.out.println("The Current Active Threads in main mathod"+Thread.currentThread().getName() );
+			System.out.println("Hello.....bro");
+
+		}
+
+
+}
