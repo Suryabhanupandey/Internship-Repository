3a44d8c8995ed0fb23a712584463663fd7684fd6
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2026-01-10 22:54:35 +0530

    templete-literals

---
+++ JAVASCRIPT/DAY15/template.literals.js
@@ -0,0 +1,19 @@
+/*
+Template Literals in JavaScript
+    Definition:
+        Template literals are a modern way to work with strings in JavaScript.
+        They allow you to:
+        Write multi-line strings
+        Use variables or expressions easily inside strings
+    🔹 Syntax:
+        Template literals use backticks (`) instead of quotes (' or ").
+*/
+ let age = prompt("Enter Age");
+ let name = prompt("enter name");
+console.log(`Name : ${name} Age ${age}`);
+let box =document.getElementsByClassName('conatiner');
+let p = document.createElement('p');
+p.innerHTML="Hello";
+p.setAttribute("class" ,"para");
+console.log(p);
+
