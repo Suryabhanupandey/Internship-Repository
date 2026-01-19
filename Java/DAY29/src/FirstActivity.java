21662a646bac1dfb2e2c07edf82838e12d6ac8ce
Suryabhanu Pandey <suryabhanupandey08@gmail.com>
2026-01-19 12:31:57 +0530

    DAY29

--- Java/DAY29/src/FirstActivity.java
+++ Java/DAY29/src/FirstActivity.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+package com.app;
+
+import java.util.*;
 
 public class FirstActivity {
+
+    public static void main(String[] args) {
+
+        // Collection (ArrayList)
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Ram");
+        names.add("Shyam");
+        names.add("Mohan");
+        names.add("Sohan");
+
+        // Iterator
+        Iterator<String> it = names.iterator();
+
+        // Removing element using iterator
+        while (it.hasNext()) {
+            String name = it.next();
+
+            if (name.equals("Mohan")) {
+                it.remove(); // safe remove
+            }
+        }
+
+        // Final list
+        System.out.println(names);
+    }
 }
